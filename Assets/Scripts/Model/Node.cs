57a3c5a5c6585bf1055fd7689edb4b323b723882
ChangHyeon Kim <hazasue@gmail.com>
2024-05-17 16:46:44 +0900

    :tada: Init Node script

---
+++ Assets/Scripts/Model/Node.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public int file;
+    public float time;
+    public KeyCode key;
+    public int targetCharacter;
+    public int animationIdx;
+    public string sfxPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
