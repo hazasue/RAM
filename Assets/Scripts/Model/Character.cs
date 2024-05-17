0ed2cbd9693209e5e61cb75b055ab4ed3ac0d833
ChangHyeon Kim <hazasue@gmail.com>
2024-05-17 16:46:28 +0900

    :tada: Init Character script

---
+++ Assets/Scripts/Model/Character.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int id;
+    public string imagePath;
+
+    public List<Animation> animations;
+    
+    public void PlayAnimation(int idx) {}
+}
