22e7db024620c1abc103fb15803df2a2f84b2157
ChangHyeon Kim <hazasue@gmail.com>
2024-05-20 12:59:41 +0900

    :hammer: GameFileInstance returns key value

--- Assets/Scripts/Model/GameFileInstance.cs
+++ Assets/Scripts/Model/GameFileInstance.cs
@@ -6,14 +6,19 @@ using TMPro;
 
 public class GameFileInstance : MonoBehaviour
 {
+    private int key;
+    
     public TMP_Text id;
     public TMP_Text name;
     public TMP_Text description;
 
     public void Init(int id, string name, string description)
     {
+        this.key = id;
         this.id.text = $"{id}";
         this.name.text = $"{name}";
         this.description.text = $"{description}";
     }
+
+    public void SelectFile() { GameFileManager.currentFileKey = key; }
 }
