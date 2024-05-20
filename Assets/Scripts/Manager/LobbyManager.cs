557714dad2da493a59bb87661e92979a57c91daa
ChangHyeon Kim <hazasue@gmail.com>
2024-05-20 12:59:11 +0900

    :tada: Init LobbyManager

---
+++ Assets/Scripts/Manager/LobbyManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviour
+{
+    private Stack<GameObject> screens;
+    public GameObject createScreen;
+
+    public TMP_InputField name;
+    public TMP_InputField description;
+    public TMP_InputField bgmPath;
+    
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) PopScreen();
+    }
+
+    private void init()
+    {
+        screens = new Stack<GameObject>();
+    }
+
+    public void PushScreen(string name)
+    {
+        switch (name)
+        {
+            case "create":
+                screens.Push(createScreen);
+                createScreen.SetActive(true);
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    public void LoadScene(string name)
+    {
+        switch (name)
+        {
+            case "Create":
+                SceneManager.LoadScene("Create");
+                break;
+            
+            case "Play":
+                SceneManager.LoadScene("Play");
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    public void CreateFile()
+    {
+        GameFileManager.GetInstance().CreateGameFile(name.text, description.text, bgmPath.text);
+        name.text = "";
+        description.text = "";
+        bgmPath.text = "";
+    }
+
+    public void PopScreen()
+    {
+        if (screens.Count <= 0) return;
+
+        screens.Pop().SetActive(false);
+    }
+    
+}
