0959a3e6e2ce2b23fe100d480bb27631ac8aeebd
ChangHyeon Kim <hazasue@gmail.com>
2024-05-21 20:23:01 +0900

    :hammer: Apply key inputs

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     private const int MAX_KEY_COUNT = 4;
-    
     private List<KeyCode> keys;
-    
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -17,10 +16,23 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        applyKeyInput();
     }
-    
-    private void init() {}
-    
-    private void applyKeyInput() {}
+
+    private void init()
+    {
+        keys = new List<KeyCode>();
+    }
+
+    private void applyKeyInput()
+    {
+        keys.Clear();
+
+        if (Input.GetKeyDown(KeyCode.D)) keys.Add(KeyCode.D);
+        if (Input.GetKeyDown(KeyCode.F)) keys.Add(KeyCode.F);
+        if (Input.GetKeyDown(KeyCode.J)) keys.Add(KeyCode.J);
+        if (Input.GetKeyDown(KeyCode.K)) keys.Add(KeyCode.K);
+
+        PatternManager.GetInstance().HandleKeyInput(keys);
+    }
 }
