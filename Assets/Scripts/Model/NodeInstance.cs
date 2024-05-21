b3ecd7d0c887faa2e2c738135e411879ac5c620c
ChangHyeon Kim <hazasue@gmail.com>
2024-05-21 20:21:57 +0900

    :tada: Init NodeInstance

---
+++ Assets/Scripts/Model/NodeInstance.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeInstance : MonoBehaviour
+{
+    public static float MAGNIFICATION = 8f;
+    
+    public float time;
+    public KeyCode key;
+
+    void Update()
+    {
+        this.transform.position += new Vector3(0f, PatternManager.DEFAULT_NODE_SIZE * PatternManager.MAGNIFICATION * Time.deltaTime, 0f);
+    }
+
+    public void Init(float time, KeyCode key, float xPos, Transform parent)
+    {
+        this.time = time;
+        this.key = key;
+        this.transform.parent = parent;
+
+        this.transform.localPosition = new Vector3(xPos, -time * PatternManager.DEFAULT_NODE_SIZE * PatternManager.MAGNIFICATION, 0f);
+    }
+}
