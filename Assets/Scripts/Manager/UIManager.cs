3d886f3e446864ac5f9a94709173fac7a68f5fa5
ChangHyeon Kim <hazasue@gmail.com>
2024-05-21 20:22:24 +0900

    :hammer: Update timer

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    private static UIManager instance;
+    
     public Slider progress;
     public Slider hp;
+    public TMP_Text timer;
     
     // Start is called before the first frame update
     void Start()
@@ -20,7 +24,20 @@ public class UIManager : MonoBehaviour
         
     }
     
+    public static UIManager GetInstance()
+    {
+        if (instance != null) return instance;
+        instance = FindObjectOfType<UIManager>();
+        if (instance == null) Debug.Log("There's no active UIManager object");
+        return instance;
+    }
+    
     public void Init(float maxHp) {}
     
     public void UpdateSlider(float pregress, float currentHp) {}
+
+    public void UpdateTimer(float time)
+    {
+        timer.text = $"{((int)(time / 60)).ToString("D2")} : {((int)(time % 60)).ToString("D2")} : {((int)(time % 1f * 100f)).ToString("D2")}";
+    }
 }
