98facb347cf6b5c0985b610dcf4acfb5d85d0a97
ChangHyeon Kim <hazasue@gmail.com>
2024-05-21 22:38:36 +0900

    :fire: Remove useless variable

--- Assets/Scripts/Model/GameFileInstance.cs
+++ Assets/Scripts/Model/GameFileInstance.cs
@@ -10,14 +10,12 @@ public class GameFileInstance : MonoBehaviour
     
     public TMP_Text id;
     public TMP_Text name;
-    public TMP_Text description;
 
-    public void Init(int id, string name, string description)
+    public void Init(int id, string name)
     {
         this.key = id;
         this.id.text = $"{id}";
         this.name.text = $"{name}";
-        this.description.text = $"{description}";
     }
 
     public void SelectFile() { GameFileManager.currentFileKey = key; }
