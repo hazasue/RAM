da3b97c18ed55c34a5a196dc117bee8f2e0ce718
ChangHyeon Kim <hazasue@gmail.com>
2024-05-26 21:29:37 +0900

    :hammer: Save auto play boolean

--- Assets/Scripts/Model/CurrentInfo.cs
+++ Assets/Scripts/Model/CurrentInfo.cs
@@ -7,10 +7,12 @@ public class CurrentInfo
 {
     public int id;
     public bool createMode;
+    public bool autoPlay;
 
-    public CurrentInfo(int id, bool createMode)
+    public CurrentInfo(int id, bool createMode, bool autoPlay = false)
     {
         this.id = id;
         this.createMode = createMode;
+        this.autoPlay = autoPlay;
     }
 }
