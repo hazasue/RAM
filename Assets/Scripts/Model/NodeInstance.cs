d6bc93af498a754cb69709d00717cb26e5ce97f6
ChangHyeon Kim <hazasue@gmail.com>
2024-05-26 20:26:50 +0900

    :hammer: Can toggle spawn time

--- Assets/Scripts/Model/NodeInstance.cs
+++ Assets/Scripts/Model/NodeInstance.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NodeInstance : MonoBehaviour
 {
@@ -21,5 +22,27 @@ public class NodeInstance : MonoBehaviour
         this.transform.SetParent(parent);
 
         this.transform.localPosition = new Vector3(xPos, -time * PatternManager.DEFAULT_NODE_SIZE * PatternManager.MAGNIFICATION, 0f);
+    }
+    
+    public void InitMaking(float time, KeyCode key, float xPos, Transform parent)
+    {
+        this.time = time;
+        this.key = key;
+        this.transform.SetParent(parent);
+
+        this.transform.localPosition = new Vector3(xPos, 0f, 0f);
+    }
+
+    public void UpdateCurrentSelectedNode()
+    {
+        PatternManager.GetInstance().UpdateMakingNodeInfo(this);
+    }
+
+    public void ToggleTime(float scale)
+    {
+        time += scale;
+
+        this.transform.position -=
+            new Vector3(0f, PatternManager.DEFAULT_NODE_SIZE * PatternManager.MAGNIFICATION * scale, 0f);
     }
 }
