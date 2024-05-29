b69aee604b7365634d15bd647de9cf98d7d6aeb0
ChangHyeon Kim <hazasue@gmail.com>
2024-05-29 21:40:55 +0900

    :hammer: Node saves duration info

--- Assets/Scripts/Model/Node.cs
+++ Assets/Scripts/Model/Node.cs
@@ -7,15 +7,17 @@ public class Node
 {
     public int file;
     public float time;
+    public float duration;
     public KeyCode key;
     public int targetCharacter;
     public int animationIdx;
     public string sfxPath;
 
-    public Node(int file, float time, KeyCode key, int targetCharacter, int animationIdx, string sfxPath)
+    public Node(int file, float time, float duration, KeyCode key, int targetCharacter, int animationIdx, string sfxPath)
     {
         this.file = file;
         this.time = time;
+        this.duration = duration;
         this.key = key;
         this.targetCharacter = targetCharacter;
         this.animationIdx = animationIdx;
