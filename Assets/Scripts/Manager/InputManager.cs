fdf3530240dd8b0799b432630f33c6c1c5c83298
ChangHyeon Kim <hazasue@gmail.com>
2024-05-29 21:41:52 +0900

    :hammer: Save keys as dict type

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class InputManager : MonoBehaviour
 {
     private const int MAX_KEY_COUNT = 4;
-    private List<KeyCode> keys;
+    private Dictionary<KeyCode, bool> keys;
 
     // Start is called before the first frame update
     void Awake()
@@ -21,17 +22,22 @@ public class InputManager : MonoBehaviour
 
     private void init()
     {
-        keys = new List<KeyCode>();
+        keys = new Dictionary<KeyCode, bool>();
     }
 
     private void applyKeyInput()
     {
         keys.Clear();
-
-        if (Input.GetKeyDown(KeyCode.D)) keys.Add(KeyCode.D);
-        if (Input.GetKeyDown(KeyCode.F)) keys.Add(KeyCode.F);
-        if (Input.GetKeyDown(KeyCode.J)) keys.Add(KeyCode.J);
-        if (Input.GetKeyDown(KeyCode.K)) keys.Add(KeyCode.K);
+        
+        if (Input.GetKeyDown(KeyCode.D)) keys.Add(KeyCode.D, true);
+        if (Input.GetKeyDown(KeyCode.F)) keys.Add(KeyCode.F, true);
+        if (Input.GetKeyDown(KeyCode.J)) keys.Add(KeyCode.J, true);
+        if (Input.GetKeyDown(KeyCode.K)) keys.Add(KeyCode.K, true);
+        
+        if (Input.GetKeyUp(KeyCode.D)) keys.Add(KeyCode.D, false);
+        if (Input.GetKeyUp(KeyCode.F)) keys.Add(KeyCode.F, false);
+        if (Input.GetKeyUp(KeyCode.J)) keys.Add(KeyCode.J, false);
+        if (Input.GetKeyUp(KeyCode.K)) keys.Add(KeyCode.K, false);
 
         PatternManager.GetInstance().HandleKeyInput(keys);
     }
